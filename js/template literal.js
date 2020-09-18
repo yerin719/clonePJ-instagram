db56604202e0702d760caa17c3a2967f99f471b0
yerin719 <59353169+yerin719@users.noreply.github.com>
2020-09-18 15:16:15 +0900

    init

---
+++ js/template literal.js
@@ -0,0 +1,5 @@
+let constant = 2;
+let string1="banana";
+let string2="tomato";
+
+console.log(`I have ${constant} ${string1} ${string2}`);
---
+++ js/val.js
@@ -0,0 +1,35 @@
+// var abc = "test";
+var abc;
+abc = "test";
+function tttt() {
+  var abc = 123;
+  //   abc = 123;
+  console.log("functoin:", abc);
+}
+tttt();
+console.log(abc);
+let abc;
+abc = "test";
+function tttt() {
+//   let abc = 123;
+//   abc = 123;
+  console.log("functoin:", abc);
+}
+tttt();
+console.log(abc);
+const abc;
+abc = "test"
+const abc = "test";
+function tttt() {
+  //   const abc = 123;
+  abc = 123;
+  console.log("function:", abc);
+}
+tttt();
+console.log(abc);
+smile = "hellos";
+console.log("before:", smile);
+// var smile = ":-)";
+let smile = ":-)";
+const smile = ":-)";
+console.log("after: ", smile);
